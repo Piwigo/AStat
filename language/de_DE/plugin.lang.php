fb1d8dc3d837bc25f242294c81a8be5dff500927
duke <duke@piwigo.org>
2010-04-11 12:20:13 +0000

    added language de_DE
    git-svn-id: http://piwigo.org/svn/extensions/AStat@5794 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,220 @@
+<?php
+
+//--- >> v1.1.1 >>
+$lang['Pictures_seen'] = 'Betrachtete Bilder';
+$lang['Categories_seen'] = 'Betrachtete Kategorien';
+$lang['IP_visit'] = 'IP Adressen';
+$lang['AStat_title_page'] = 'Erweiterte Statistiken';
+$lang['IP_label'] = 'IP Adresse';
+$lang['CATEGORY_LABEL'] = 'Kategorie';
+$lang['pct_Pictures_seen'] = '%Bilder';
+$lang['pct_Pages_seen'] = '%Seiten';
+$lang['ratio_Pictures_seen'] = 'Bilder betrachtet';
+
+$lang['AStat_period_label_hours'] = 'Stunden';
+$lang['AStat_period_label_days'] = 'Tage';
+$lang['AStat_period_label_months'] = 'Monate';
+$lang['AStat_period_label_years'] = 'Jahre';
+$lang['AStat_period_label_all'] = 'Ganzes Jahr';
+$lang['AStat_period_label_global'] = 'Global';
+
+
+$lang['AStat_day_of_week_0'] = 'Sonntag'; //sunday
+$lang['AStat_day_of_week_1'] = 'Montag';
+$lang['AStat_day_of_week_2'] = 'Dienstag';
+$lang['AStat_day_of_week_3'] = 'Mittwoch';
+$lang['AStat_day_of_week_4'] = 'Donnerstag';
+$lang['AStat_day_of_week_5'] = 'Freitag';
+$lang['AStat_day_of_week_6'] = 'Samstag';
+
+$lang['AStat_month_of_year_1'] = 'Jänner';
+$lang['AStat_month_of_year_2'] = 'Februar';
+$lang['AStat_month_of_year_3'] = 'März';
+$lang['AStat_month_of_year_4'] = 'April';
+$lang['AStat_month_of_year_5'] = 'Mai';
+$lang['AStat_month_of_year_6'] = 'Juni';
+$lang['AStat_month_of_year_7'] = 'Juli';
+$lang['AStat_month_of_year_8'] = 'August';
+$lang['AStat_month_of_year_9'] = 'September';
+$lang['AStat_month_of_year_10'] = 'Oktober';
+$lang['AStat_month_of_year_11'] = 'November';
+$lang['AStat_month_of_year_12'] = 'Dezember';
+
+$lang['AStat_by_period'] = 'Nach Zeitraum';
+$lang['AStat_by_ip'] = 'Nach IP';
+$lang['AStat_by_category'] = 'Nach Kategorie';
+$lang['AStat_config'] = 'Einstellungen';
+
+$lang['AStat_BarColor_Pages'] = 'Seitenanzahl';
+$lang['AStat_BarColor_Img'] = 'Bilderanzahl';
+$lang['AStat_BarColor_IP'] = 'Anzahl IP Adressen';
+$lang['AStat_MouseOverColor'] = 'Mouse-over Spalte';
+$lang['AStat_config_title'] = 'AStat-Einstellungen';
+$lang['AStat_config_colors_and_graph'] = 'Farb- und Grafikeinstellungen';
+$lang['AStat_specific_ip_config'] = 'Einstellungen für Statistiken nach IP Adressen';
+$lang['AStat_NbIPPerPages'] = 'Anzahl der IP Adressen pro Seite';
+$lang['AStat_MaxBarWidth'] = 'Maximale Balkenbreite (Pixel)';
+$lang['AStat_NbCatPerPages'] = 'Anzahl der Kategorien pro Seite';
+$lang['AStat_specific_category_config'] = 'Einstellungen für Statistiken nach Kategorien';
+$lang['AStat_do_save'] = 'Speichern';
+
+$lang['AStat_page_label'] = 'Seite';
+$lang['AStat_pages_label'] = 'Seiten';
+$lang['AStat_nb_total_ip'] = 'Gesamtanzahl von IP Adressen';
+$lang['AStat_nb_total_category'] = 'Gesamtanzahl von Kategorien';
+
+$lang['AStat_Nfo_Category'] = "Für den angegebenen Zeitraum wird die Anzahl der Seiten- und Bildaufrufe pro Kategorie in Prozent und die mittlere Anzahl der Bildaufrufe dargestellt";
+$lang['AStat_Nfo_IP'] = "Für den angegebenen Zeitraum wird die Anzahl der Seiten- und Bildaufrufe pro IP Adresse dargestellt";
+$lang['AStat_Nfo_Period'] = "Für den angegebenen Zeitraum wird die Anzahl der Seiten- und Bildaufrufe sowie die Anzahl unterschiedlicher IP Adressen dargestellt";
+
+$lang['AStat_IP_geolocalisation'] = 'Geolokalisierung';
+$lang['AStat_PeriodPerDefault'] = 'Standardmäßig eingestellter Zeitraum';
+$lang['AStat_specific_period_config'] = 'Einstellungn für Statistiken nach Zeitraum';
+$lang['AStat_PeriodPerDefault_global'] = 'Global';
+$lang['AStat_PeriodPerDefault_all'] = 'Alle Jahre';
+$lang['AStat_PeriodPerDefault_year'] = 'Das aktuelle Jahr';
+$lang['AStat_PeriodPerDefault_month'] = 'Der aktuelle Monat';
+$lang['AStat_PeriodPerDefault_day'] = 'Der aktuelle Tag';
+$lang['AStat_config_saved'] = 'Die Einstellungen wurden gespeichert!';
+$lang['AStat_ShowThumbCat'] = 'Zeige das der Kategorie zugewiesene Miniaturbild';
+$lang['AStat_yesno_true'] = 'Ja';
+$lang['AStat_yesno_false'] = 'Nein';
+$lang['AStat_sortcat_page'] = 'Prozentuelle Darstellung der aufgerufenen Seiten';
+$lang['AStat_sortcat_picture'] = 'Prozentuelle Darstellung der aufgerufenen Bilder';
+$lang['AStat_sortcat_nbpicture'] = 'Durchschnittliche Anzahl der Bildaufrufe';
+$lang['AStat_DefaultSortCat'] = 'Standardsortierung';
+$lang['AStat_SortCatLabel'] = 'Sortierung nach ';
+$lang['AStat_version'] = 'version ';
+
+$lang['AStat_RefImageLabel'] = 'Kategorie / Bildname';
+$lang['AStat_by_image'] = 'Pro Bild';
+$lang['AStat_nb_total_image'] = 'Gesamtanzahl der Bilder';
+$lang['AStat_SortImgLabel'] = 'Sortiert nach ';
+$lang['AStat_sortimg_picture'] = 'Anzahl der Bildaufrufe';
+$lang['AStat_sortimg_catname'] = 'Alphabetische Auflistung - Kategorie / Bildname';
+$lang['AStat_Nfo_Image'] = "Für den angegebenen Zeitraum werden die Aufrufe jedes Bildes im Verhältnis zum Aufruf aller Fotos dargestellt (in Prozent)";
+$lang['AStat_specific_image_config'] = 'Einstellungen für Statistiken nach Bildern';
+$lang['AStat_NbImgPerPages'] = 'Anzahl Bilder pro Seite';
+$lang['AStat_ShowThumbImg'] = 'Zeige das Miniaturbild des Fotos';
+$lang['AStat_DefaultSortImg'] = 'Standardsortierung';
+
+//--- << v1.1.1 <<
+//--- >> v1.2.0 >>
+$lang['AStat_BarColor_Cat'] = 'Kategorieanzahl';
+$lang['AStat_deleted_user'] = 'Gelöschte Benutzer';
+$lang['AStat_SortIPLabel'] = 'Sortierung nach ';
+$lang['AStat_sortip_picture'] = 'Anzahl aufgerufener Bilder';
+$lang['AStat_sortip_ip'] = 'Benutzer / IP Adresse';
+$lang['AStat_sortip_page'] = 'Anzahl aufgerufener Seiten';
+$lang['AStat_RefIPLabel'] = 'Benutzer / IP Adresse';
+$lang['AStat_DefaultSortIP'] = 'Standardsortierung';
+$lang['AStat_general_config'] = 'Globale Einstellungen';
+$lang['AStat_SeeTimeRequests'] = 'Zeige Verarbeitungsdauer der Anfragen';
+$lang['AStat_time_request_label'] = 'Verarbeitungsdauer der Anfrage';
+
+$lang['AStat_section_categories'] = $lang['Categories'];
+$lang['AStat_section_tags'] = $lang['Tags'];
+$lang['AStat_section_search'] = $lang['search'];
+$lang['AStat_section_list'] = $lang['random_cat'] ; // correction v1.2.1
+$lang['AStat_section_favorites'] = $lang['favorites'];
+$lang['AStat_section_most_visited'] = $lang['most_visited_cat'];
+$lang['AStat_section_best_rated'] = $lang['best_rated_cat'];
+$lang['AStat_section_recent_pics'] = $lang['recent_pics_cat'];
+$lang['AStat_section_recent_cats'] = $lang['recent_cats_cat'];
+$lang['AStat_section_additional_page'] = 'Plugin <a href="http://phpwebgallery.net/ext/extension_view.php?eid=153">Additional pages</a>';
+$lang['AStat_section_web_services'] = 'Plugin <a href="http://phpwebgallery.net/ext/extension_view.php?eid=171">Web services statistics</a>';
+$lang['AStat_section_most_commented'] = 'Plugin <a href="http://phpwebgallery.net/ext/extension_view.php?eid=145">Most Commented</a>';
+$lang['AStat_section_unknown'] = 'Unbekannt [%s]';
+
+$lang['AStat_date_time_format'] = 'Y-m-d H:i:s';
+$lang['AStat_event'] = 'Ereignis';
+$lang['AStat_events'] = 'Ereignisse';
+$lang['AStat_section_label'] = 'Sektion';
+$lang['AStat_deleted_picture'] = 'Bild existiert nicht mehr';
+$lang['AStat_tools'] = 'Werkzeuge';
+$lang['AStat_tools_title'] = 'Werkzeuge für die Systemwartung';
+$lang['AStat_tools_result_ok'] = "Aktionen wurden erfolgreich durchgeführt";
+
+$lang['AStat_tools_deleted_user'] = 'Aktualisieren der Historie mit den Verweisen des Benutzers'; //v1.3.0
+$lang['AStat_tools_deleted_user_nfo0'] = "Diese Funktion versetzt einen Benutzer-ID, der aufgrund eines Eintrags in der Historie erstellt und mittlerweile wieder gelöscht wurde, in den Status <i>Gast</i>.";
+$lang['AStat_tools_deleted_user_nfo1'] = "%s unbekannte Benutzer-IDs in der Historie in Verbindung mit %s Ereignissen : <ul>%s</ul>";
+$lang['AStat_tools_deleted_user_nfo2'] = "Alle Benutzer-IDs in der Historie dieses Ereignisses existieren, daher wird keine Aktion unternommen.";
+$lang['AStat_tools_deleted_user_apply'] = 'Update';
+
+$lang['AStat_tools_general_nfo'] = 'Allgemeine Information zur Historie';
+
+$lang['AStat_tools_deleted_picture'] = 'Aktualisieren der Historie mit Bildreferenzen'; // v1.3.0
+$lang['AStat_tools_deleted_picture_nfo0'] = "Ereignisse in der Historie in Verbindung mit Bildern, die nicht mehr existieren, ein <i>Bild [Id #0] existiert nicht mehr</i>"; // v1.3.0
+$lang['AStat_tools_deleted_picture_nfo1'] = "%s unbekannte Bild-IDs in der Ereignishistorie in Verbdindung mit %s Ereignissen : <ul>%s</ul>";
+$lang['AStat_tools_deleted_picture_nfo2'] = "Alle Bild-IDs in Verbindung mit diesem Ereignis existieren, daher wird keine Aktion unternommen.";
+$lang['AStat_tools_deleted_picture_apply'] = 'Update';
+
+//--- << v1.2.0 <<
+
+//--- >> v1.3.0 >>
+
+$lang['AStat_section_old_deleted_cat'] = 'Die alte Kategorie kann nicht importiert werden ';
+$lang['AStat_tools_general_nfo_nfo'] = "<ul><li>%s Ereignisse sind in der Historie gespeichert</li>
+            <li>Tabellengewichtung %s (Tabelle: %s ; Index: %s)</li>
+            <li>Datum des ersten Ereignisses : %s</li>
+            <li>Datum des letzten Ereignisses : %s</li>";
+$lang['AStat_tools_purge_history'] = "Löschen der Historie";
+$lang['AStat_tools_purge_history_nfo'] = "Diese Funktion löscht alle vergangenen Ereignisse ab einem bestimmten Datum und optimiert die Tabelle.<br><b>Dieser Schritt kann nicht rückgängig gemacht werden</b>.";
+$lang['AStat_tools_purge_history_apply'] = "Löschen";
+$lang['AStat_tools_purge_history_date'] = "Löschen aller Ereignisse vor ";
+$lang['AStat_tools_purge_history_imageid0'] = "Löschen aller Ereignisse in Verbindung mit gelöschten Bildern (%s Ereignisse in der Historie)";
+$lang['AStat_tools_purge_history_categoryid0'] = "Löschen aller Ereignisse in Verbindung mit gelöschten Kategorien (%s Ereignisse in der Historie)";
+$lang['AStat_tools_result_ko'] = "Ein Fehler ist aufgetreten, die Maßnahme konnte nicht ordnungsgemäß durchgeführt werden";
+$lang['AStat_tools_invalid_date'] = "Das angegebene Datum ist nicht gültig !";
+$lang['AStat_tools_invalid_date2'] = "Das angegebene Datum ist der heutige Tag oder liegt in der Zukunft";
+$lang['AStat_tools_deleted_picture_error0'] = "Die Vorbereitungen sind fehlgeschlagen";
+$lang['AStat_tools_deleted_picture_error1'] = "Die Neuverknüpfung der Ereignisse in der Historie ist fehlgeschlagen";
+$lang['AStat_tools_deleted_picture_ok0'] = "Die Vorbereitung der Tabelle wurde erfolgreich abgeschlossen, die Dateien auf dem Server können neu organisiert werden";
+$lang['AStat_tools_deleted_picture_ok1'] = "Die Verknüpfung der Ereignisse in der Historie wurde erfolgreich abgeschlossen";
+
+$lang['AStat_tools_deleted_picture_nfo3'] = "Die Synchronisation löscht Bilder, die physisch verschoben wurden, aus der Basis und legt sie mit einer neuen Referenz an. Ein verschobenes Bild hinterlässt also Ereignisse mit ungültiger Referenz in der Historie.</p><p><i>Astat</i> erlaubt :<ul><li>Speichern von Bildreferenzen <u><i>vor</i></u> der Synchronisation</li><li>Verknüpfen von Ereignissen in der Historie mit der korrekten Bildreferenz <u><i>nach</i></u> einer Synchronisation</li></p><p><b>Achtung !</b><br><ul><li>Haben Bilder den selben Dateinamen ist das Ergebnis nicht vorhersagbar</li><li>Diese Funktion kann nicht mit Bildern umgehen, deren Dateinamen auf dem Server verändert werden</li></ul>";
+
+$lang['AStat_tools_deleted_picture_prepare_action'] = "<u>Vor</u> der Synchronisation : bereite <i>AStat</i> vor";
+$lang['AStat_tools_deleted_picture_do_action'] = "<u>Nach</u> der Synchronisation : Verbinde erneut die Bild-ID mit Ereignissen in der Historie";
+$lang['AStat_tools_deleted_picture_do'] = "Starten";
+$lang['AStat_tools_deleted_picture_nfo_nb'] = "(%s Bild-IDs können nicht mit %s Ereignissen in der Historie verbunden werden)";
+
+
+$lang['AStat_tools_deleted_category'] = 'Aktualisiere Kategorien in der Historie';
+$lang['AStat_tools_deleted_category_nfo0'] = "Ereignisse in der Historie, die sich auf gelöschte Kategorien referenzieren"; // v1.3.0
+$lang['AStat_tools_deleted_category_nfo1'] = "%s nicht existierende Verzeichnisse in der Historie werden von %s Ereignissen referenziert : <ul>%s</ul>";
+$lang['AStat_tools_deleted_category_nfo2'] = "Alle Ereigniskategorien sind in der Historie vorhanden.";
+$lang['AStat_tools_deleted_category_apply'] = "Aktualisieren";
+
+
+$lang['AStat_section_deleted_cat'] = "Kategorie gelöscht";
+
+
+//--- << v1.3.0 <<
+//--- >> v1.3.2 >>
+$lang['AStat_adviser_not_authorized'] = "Vorgang abgebrochen: Das Beraterprofil besitzt nicht die benötigten Berechtigungen";
+//--- << v1.3.2 <<
+//--- >> v1.4.0 >>
+$lang['AStat_catfilter_list'] = "Wende Filter auf Kategorie an";
+$lang['AStat_nofilter'] = "--- Kein Filter ---";
+//--- << v1.4.0 <<
+
+//--- >> v2.0.0 >>
+$lang['AStat_confignotsaved'] = "Es ist ein Fehler aufgetreten, die Einstellungen wurden nichtgespeichert";
+$lang['AStat_gpc2_not_installed'] = "Plugin 'Grum Plugins Classes 2' (release >= %s) wird für AStat benötigt";
+
+//--- >> v2.1.0 >>
+$lang['AStat_BlackListedIP'] = "Gesperrte IP";
+$lang['AStat_AddIP'] = "In Sperrliste aufnehmen";
+$lang['AStat_DelIP'] = "Aus Sperrliste entfernen";
+$lang['AStat_tools_purge_history_ipid0'] = "Entferne Ereignisse die mit einer IP aus der Sperrliste referenziert sind (%s Einträge in der Sperrliste, %s Ereignisse in der Historie)";
+$lang['AStat_IP_blacklist'] = "In Sperrliste aufnehmen";
+$lang['AStat_enableddisabled_true'] = "Filter aktivieren";
+$lang['AStat_enableddisabled_false'] = "Filter deaktivieren";
+$lang['AStat_enableddisabled_invert'] = "Filter invertieren";
+
+//--- > 2.2.0
+$lang['To install this plugin, you need to install Grum Plugin Classes %s before'] = 'Um diesen Plugin zu installieren, müssen Sie zuerst Grum Plugin Classes %s installieren';
+
+
+?>
