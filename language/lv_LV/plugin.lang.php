e219a26d7665cf3b434dee408bfc989b25836071
ddtddt <ddtddt@piwigo.org>
2010-12-01 20:52:29 +0000

    [extensions] - AStat -  add lv_LV (Latvian) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/AStat@7965 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,223 @@
+<?php
+
+//--- >> v1.1.1 >>
+$lang['Pictures_seen'] = 'Apskatītie attēli';
+$lang['Categories_seen'] = 'Apskatītās kategorijas';
+$lang['IP_visit'] = 'IP adreses';
+$lang['AStat_title_page'] = 'Izvērstā Statistika';
+$lang['IP_label'] = 'IP adreses';
+$lang['CATEGORY_LABEL'] = 'Kategorija';
+$lang['pct_Pictures_seen'] = '%Attēli';
+$lang['pct_Pages_seen'] = '%Lapas';
+$lang['ratio_Pictures_seen'] = 'Apskatītie attēli';
+
+$lang['AStat_period_label_hours'] = 'Stundas';
+$lang['AStat_period_label_days'] = 'Dienas';
+$lang['AStat_period_label_months'] = 'Mēneši';
+$lang['AStat_period_label_years'] = 'Gadi';
+$lang['AStat_period_label_all'] = 'Visu gadu';
+$lang['AStat_period_label_global'] = 'Globāls';
+
+
+$lang['AStat_day_of_week_0'] = 'Svētdiena'; //svētdiena
+$lang['AStat_day_of_week_1'] = 'Pirmdiena';
+$lang['AStat_day_of_week_2'] = 'Otrdiena';
+$lang['AStat_day_of_week_3'] = 'Trešdien';
+$lang['AStat_day_of_week_4'] = 'Ceturtdiena';
+$lang['AStat_day_of_week_5'] = 'Piektdiena';
+$lang['AStat_day_of_week_6'] = 'Sestdiena';
+
+$lang['AStat_month_of_year_1'] = 'Janvāris';
+$lang['AStat_month_of_year_2'] = 'Februāris';
+$lang['AStat_month_of_year_3'] = 'Marts';
+$lang['AStat_month_of_year_4'] = 'Aprīlis';
+$lang['AStat_month_of_year_5'] = 'Maijs';
+$lang['AStat_month_of_year_6'] = 'Jūnijs';
+$lang['AStat_month_of_year_7'] = 'Jūlijs';
+$lang['AStat_month_of_year_8'] = 'Augusts';
+$lang['AStat_month_of_year_9'] = 'Septembris';
+$lang['AStat_month_of_year_10'] = 'Oktobris';
+$lang['AStat_month_of_year_11'] = 'Novembris';
+$lang['AStat_month_of_year_12'] = 'Decembris';
+
+$lang['AStat_by_period'] = 'Par periodu';
+$lang['AStat_by_ip'] = 'Par IP';
+$lang['AStat_by_category'] = 'Par kategoriju';
+$lang['AStat_config'] = 'Iestatījumi';
+
+$lang['AStat_BarColor_Pages'] = 'Lapu skaits';
+$lang['AStat_BarColor_Img'] = 'Attēlu skaits';
+$lang['AStat_BarColor_IP'] = 'IP adrešu skaits';
+$lang['AStat_MouseOverColor'] = 'Peles pārvilciena rinda';
+$lang['AStat_config_title'] = 'AStat iestatījumi';
+$lang['AStat_config_colors_and_graph'] = 'Krāsu & grafikas iestatījumi';
+$lang['AStat_specific_ip_config'] = 'Statistikas pēc IP detaļu iestatījumi';
+$lang['AStat_NbIPPerPages'] = 'IP adrešu skaits lapā';
+$lang['AStat_MaxBarWidth'] = 'Maksimālais joslas platums (pikseļi)';
+$lang['AStat_NbCatPerPages'] = 'Kategoriju skaits lapā';
+$lang['AStat_specific_category_config'] = 'Statistikas pēc kategorijas detaļu iestatījumi';
+$lang['AStat_do_save'] = 'Saglabāt';
+
+$lang['AStat_page_label'] = 'Lapa';
+$lang['AStat_pages_label'] = 'Lapas';
+$lang['AStat_nb_total_ip'] = 'IP adrešu kopējais skaits';
+$lang['AStat_nb_total_category'] = 'Kategoriju kopējais skaits';
+
+$lang['AStat_Nfo_Category'] = "Izvēlētajam periodam, izteikts procentos, lapu un attēlu skatījums pēc kategorijām, kā vidējais attēla skatījumu skaits";
+$lang['AStat_Nfo_IP'] = " Izvēlētajam periodam parādītais skatīto lapu un attēlu skaits no IP adresēm";
+$lang['AStat_Nfo_Period'] = "Izvēlētajam periodam parādītais skatīto lapu un attēlu skaits, kā   atšķirīgu ievienojušos IP adrešu skaits";
+
+$lang['AStat_IP_geolocalisation'] = 'Ģeolokalizācija';
+$lang['AStat_PeriodPerDefault'] = 'Pēc noklusējuma izvēlētais periods';
+$lang['AStat_specific_period_config'] = 'Specifiskie iestatījumi statistikai pēc perioda';
+$lang['AStat_PeriodPerDefault_global'] = 'Globāli';
+$lang['AStat_PeriodPerDefault_all'] = 'Visi gadi';
+$lang['AStat_PeriodPerDefault_year'] = 'Tekošais gads';
+$lang['AStat_PeriodPerDefault_month'] = 'Tekošais mēnesis';
+$lang['AStat_PeriodPerDefault_day'] = 'Tekošā diena';
+$lang['AStat_config_saved'] = 'Iestatījumi saglabāti!';
+$lang['AStat_ShowThumbCat'] = 'Rādīt kategoriju pārstāvošo sīkbildi';
+$lang['AStat_yesno_true'] = 'Jā';
+$lang['AStat_yesno_false'] = 'Nē';
+$lang['AStat_sortcat_page'] = 'Redzēto lapu pārstāvība procentos';
+$lang['AStat_sortcat_picture'] = 'Redzēto attēlu pārstāvība procentos';
+$lang['AStat_sortcat_nbpicture'] = 'Vidējais attēla skatījumu skaits';
+$lang['AStat_DefaultSortCat'] = 'Kārtošana pēc noklusējuma';
+$lang['AStat_SortCatLabel'] = 'Sakārtots pēc';
+$lang['AStat_version'] = 'versija ';
+
+$lang['AStat_RefImageLabel'] = 'Kategorijas/Attēla nosaukums';
+$lang['AStat_by_image'] = 'Pa attēliem';
+$lang['AStat_nb_total_image'] = 'Kopējais attēlu skaits';
+$lang['AStat_SortImgLabel'] = 'Sakārtots pēc ';
+$lang['AStat_sortimg_picture'] = 'Attēla skatījumu skaits';
+$lang['AStat_sortimg_catname'] = 'Pēc alfabēta - Kategorija /Attēla nosaukums';
+$lang['AStat_Nfo_Image'] = "Izvēlētajam periodam katra attēla skatījumu skaits, attiecināts pret kopējo skatījumu skaitu, izteikts procentos";
+$lang['AStat_specific_image_config'] = 'Specifiskie iestatījumi statistikai pēc attēliem';
+$lang['AStat_NbImgPerPages'] = 'Attēlu skaits lapā';
+$lang['AStat_ShowThumbImg'] = 'Rādīt attēla sīkbildi';
+$lang['AStat_DefaultSortImg'] = 'Kārtojums pēc noklusējuma';
+
+//--- << v1.1.1 <<
+//--- >> v1.2.0 >>
+$lang['AStat_BarColor_Cat'] = 'Kategoriju skaits';
+$lang['AStat_deleted_user'] = 'Izdzēstais lietotājs';
+$lang['AStat_SortIPLabel'] = 'Sakārtots pēc ';
+$lang['AStat_sortip_picture'] = 'Redzēto attēlu skaits';
+$lang['AStat_sortip_ip'] = 'Lietotājs / IP Adrese';
+$lang['AStat_sortip_page'] = 'Redzēto lapu skaits';
+$lang['AStat_RefIPLabel'] = 'Lietotājs / IP Adrese';
+$lang['AStat_DefaultSortIP'] = 'Kārtojums pēc noklusējuma';
+$lang['AStat_general_config'] = 'Globālie iestatījumi';
+$lang['AStat_SeeTimeRequests'] = 'Rādīt laika patēriņu pieprasījumiem';
+$lang['AStat_time_request_label'] = 'Pieprasījums izpildīts';
+
+$lang['AStat_section_categories'] = $lang['Kategorijas'];
+$lang['AStat_section_tags'] = $lang['Iezīmes'];
+$lang['AStat_section_search'] = $lang['meklēt'];
+$lang['AStat_section_list'] = $lang['gadījuma_kat'] ; // correction v1.2.1
+$lang['AStat_section_favorites'] = $lang['favorīti'];
+$lang['AStat_section_most_visited'] = $lang['vairāk_apmeklētie_kat'];
+$lang['AStat_section_best_rated'] = $lang['visaugstāk_vērtētie_kat'];
+$lang['AStat_section_recent_pics'] = $lang['nesenie_attēli_kat'];
+$lang['AStat_section_recent_cats'] = $lang['nesenās_kategorijas_kat'];
+$lang['AStat_section_additional_page'] = 'Spraudnis <a
+href="http://phpwebgallery.net/ext/extension_view.php?eid=153">Papildu lapas</a>';
+$lang['AStat_section_web_services'] = 'Spraudnis <a
+href="http://phpwebgallery.net/ext/extension_view.php?eid=171">Web servisu statistika
+</a>';
+$lang['AStat_section_most_commented'] = 'Spraudnis <a
+href="http://phpwebgallery.net/ext/extension_view.php?eid=145">Visvairāk komentēts</a>';
+$lang['AStat_section_unknown'] = 'Nezināms [%s]';
+
+$lang['AStat_date_time_format'] = 'G/d/m H:i:s';
+$lang['AStat_event'] = 'notikums';
+$lang['AStat_events'] = 'notikumi';
+$lang['AStat_section_label'] = 'Sekcija';
+$lang['AStat_deleted_picture'] = 'Attēls vairs neeksistē';
+$lang['AStat_tools'] = 'Rīki';
+$lang['AStat_tools_title'] = 'Profilakses rīki';
+$lang['AStat_tools_result_ok'] = "Darbība veikta korekti";
+
+$lang['AStat_tools_deleted_user'] = 'Atjaunināt vēsturi ar lietotāju atsaucēm'; //v1.3.0
+$lang['AStat_tools_deleted_user_nfo0'] = "Šī funkcija norāda uz <i>viesi</i> , kura lietotāja ID ir ģenerēts kā notikums pagātnē, kura rēķins ir bijis dzēsts.";
+$lang['AStat_tools_deleted_user_nfo1'] = "%s nezināms lietotāja ID starp notikumiem pagātnē, kas ietekmē %s notikumus : <ul>%s</ul>";
+$lang['AStat_tools_deleted_user_nfo2'] = "Visi lietotāju ID eksistē lietotāju vēsturē, Nekādas darbības nav jāveic.";
+$lang['AStat_tools_deleted_user_apply'] = 'Atjaunināt';
+
+$lang['AStat_tools_general_nfo'] = 'Vispārēja informācija par vēsturi';
+
+$lang['AStat_tools_deleted_picture'] = 'Atjaunināt vēsturi ar attēla referāļiem'; // v1.3.0
+$lang['AStat_tools_deleted_picture_nfo0'] = "Iespaido notikumus vēsturē, atsaucoties uz attēliem, kav vairs neeksistē, <i>attēls [Id #0] datu bāzē vairs neeksistē</i>"; // v1.3.0
+$lang['AStat_tools_deleted_picture_nfo1'] = "%s nezināma attēla ID notikumu vēsturē, kas ietekmē %s notikumus : <ul>%s</ul>";
+$lang['AStat_tools_deleted_picture_nfo2'] = "Notikumu vēsturē eksistē visu attēlu ID, nav nepieciešams veikt nekādas darbības.";
+$lang['AStat_tools_deleted_picture_apply'] = 'Atjaunināt';
+
+//--- << v1.2.0 <<
+
+//--- >> v1.3.0 >>
+
+$lang['AStat_section_old_deleted_cat'] = 'Neiespējams importēt veco kategoriju ';
+$lang['AStat_tools_general_nfo_nfo'] = "<ul><li>%s vēsturē esošie notikumi</li>
+<li>Tabulas noslodze %s (Tabula: %s ; Indekss: %s)</li>
+<li>Pirmā notikuma datums : %s</li>
+<li>Pēdējā notikuma datums : %s</li>";
+$lang['AStat_tools_purge_history'] = "Vēstures tīrīšana";
+$lang['AStat_tools_purge_history_nfo'] = "Ar šīs funkcijas palīdzību no notikumu vēstures dzēš visus notikumus izvēlētajā datumā, tiek optimizēta notikumu tabula.
+<br><b>Darbība nav atceļama</b>.";
+$lang['AStat_tools_purge_history_apply'] = "Tīrīt";
+$lang['AStat_tools_purge_history_date'] = "Iztīra visus iepriekšējos notikumus ";
+$lang['AStat_tools_purge_history_imageid0'] = "Lai notīrītu visus ar dzēstiem attēliem saistītos notikumus (%s notikumi vēsturē)";
+$lang['AStat_tools_purge_history_categoryid0'] = "Lai notīrītu ar dzēstām kategorijām saistītus notikumus (%s notikumi vēsturē)";
+$lang['AStat_tools_result_ko'] = "Radusies kļūme. Darbība veikta nekorekti";
+$lang['AStat_tools_invalid_date'] = "Iedotais datums nav korekts !";
+$lang['AStat_tools_invalid_date2'] = "Iedotais datums ir lielāks vai vienāds ar dienas datumu";
+$lang['AStat_tools_deleted_picture_error0'] = "Primārā darbība nav veiksmīga";
+$lang['AStat_tools_deleted_picture_error1'] = "Jauna notikumu sasaiste vēsturē nav veiksmīga";
+$lang['AStat_tools_deleted_picture_ok0'] = "Tabulas pārkārtojums veiksmīgs, failus var pārkārtot uz servera";
+$lang['AStat_tools_deleted_picture_ok1'] = "Notikuma sasaiste vēsturē veiksmīga";
+
+$lang['AStat_tools_deleted_picture_nfo3'] = "Kad attēli tiek fiziski pārvietotino vienas mapes, sinhronizācija dzēš attēlus no datu bāzes un atiestata tos ar jaunām atsaucēm. Tad pārvietotais attēls paliek notikumu arhīvā sasaistīts ar atsauci, kav vairs neeksistē><i>Astat</i> ļauj :<ul><li>iegaumēt
+attēlu atsauces <u><i>pirms</i></u> sinhronizēt</li><li> vēsturisko notikumu saiti ar precīzu attēla atsauci <u><i>pēc</i></u> sinhronizācijas.</li></p><p><b>Uzmanieties !</b><br><ul><li> attēliem jābūt nosauktiem tāpat kā to failiem, jo citādi iznākums nav paredzams</li><li>Šī funkcija nevar strtādāt ar attēliem, kuru faila vārdi uz servera ir izmainīti</li></ul>";
+
+$lang['AStat_tools_deleted_picture_prepare_action'] = "<u>Iepriekšējais</u> sinhronizēšanai : sagatavots <i>AStat</i>";
+$lang['AStat_tools_deleted_picture_do_action'] = "<u>Pēc</u>
+sinhronizēšanas : no jauna sasaistīt jaunā attēla ID ar notikumiem vēsturē";
+$lang['AStat_tools_deleted_picture_do'] = "Veidot procesu";
+$lang['AStat_tools_deleted_picture_nfo_nb'] = "(%s attēla Id nevar sasaistīt ar %s notikumiem vēsturē)";
+
+
+$lang['AStat_tools_deleted_category'] = 'Atjaunināt kategorijas vēsturē';
+$lang['AStat_tools_deleted_category_nfo0'] = "Piešķīrumi notikumiem vēsturē, kas vairs neekstistē. Izdzēstā kategorija"; // v1.3.0
+$lang['AStat_tools_deleted_category_nfo1'] = "%s notikumu vēsturē atrodas informācija par neeksistējošām mapēm, kas var ietekmēt %s notikumus : <ul>%s</ul>";
+$lang['AStat_tools_deleted_category_nfo2'] = "Vēsturē eksistē visu notikumu kategorijas. Nav nepieciešams veikt nekādas darbības.";
+$lang['AStat_tools_deleted_category_apply'] = "Atjaunināt";
+
+
+$lang['AStat_section_deleted_cat'] = "Kategorija dzēsta";
+
+
+//--- << v1.3.0 <<
+//--- >> v1.3.2 >>
+$lang['AStat_adviser_not_authorized'] = "Darbība atcelta : konsultanta profilam nav atļauta šāda veida darbība";
+//--- << v1.3.2 <<
+//--- >> v1.4.0 >>
+$lang['AStat_catfilter_list'] = "Pielietot filtru kategorijām";
+$lang['AStat_nofilter'] = "--- Nav filtra ---";
+//--- << v1.4.0 <<
+
+//--- >> v2.0.0 >>
+$lang['AStat_confignotsaved'] = "Radusies kļūda, iestatījumi nav saglabāti";
+$lang['AStat_gpc2_not_installed'] = "Spraudnis 'Grum Plugins Classes 2' (laidiens >= %s) ir nepieciešams, lai uzinstalētu AStat";
+
+//--- >> v2.1.0 >>
+$lang['AStat_BlackListedIP'] = "IP melnajā sarakstā";
+$lang['AStat_AddIP'] = "Pievienot melnajam sarakstam";
+$lang['AStat_DelIP'] = "Dzēst no melnā saraksta";
+$lang['AStat_tools_purge_history_ipid0'] = "Dzēst no melnā saraksta ar IP saistītos notikumus (%s vienības melnajā sarakstā, %s notikumu vēsturē)";
+$lang['AStat_IP_blacklist'] = "Pievienot melnajam sarakstam";
+$lang['AStat_enableddisabled_true'] = "Filtrs iespējots";
+$lang['AStat_enableddisabled_false'] = "Filtrs atslēgts";
+$lang['AStat_enableddisabled_invert'] = "Filtrs invertēts";
+
+?>
