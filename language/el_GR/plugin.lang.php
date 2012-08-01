d749e606f33d6357480b08467ec8a110fe1cefc9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-01 15:40:05 +0000

    [astat] Update el_GR, thanks to :
    git-svn-id: http://piwigo.org/svn/extensions/AStat@17276 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['AStat_SeeTimeRequests'] = 'Εμφάνιση χρόνου εκτέλεσης των αιτημάτων';
+$lang['AStat_RefImageLabel'] = ' Όνομα Κατηγορίας / εικόνας';
+$lang['AStat_RefIPLabel'] = 'Χρήστης / διεύθυνση IP';
+$lang['AStat_PeriodPerDefault_year'] = 'Το τρέχον έτος';
+$lang['AStat_PeriodPerDefault_month'] = 'Ο τρέχον μήνας';
+$lang['AStat_PeriodPerDefault_global'] = 'Συνολικά';
+$lang['AStat_PeriodPerDefault_day'] = 'Η τρέχουσα ημέρα';
+$lang['AStat_PeriodPerDefault_all'] = 'Όλα τα έτη';
+$lang['AStat_PeriodPerDefault'] = 'Προεπιλεγμένη περίοδος';
+$lang['AStat_Nfo_Period'] = 'Για την επιλεγμένη περίοδο, αντιπροσωπεύει τον αριθμό των σελίδων και εικόνων, ως ο αριθμός των διακεκριμένων διευθύνσεων IP που συνδέθηκαν';
+$lang['AStat_Nfo_Image'] = 'Για την επιλεγμένη περίοδο, ο αριθμός εμφανίσεων για κάθε εικόνα, ως παράσταση επί τις εκατό του αριθμού των εμφανίσεων για το συνολικό αριθμό εμφανίσεων';
+$lang['AStat_Nfo_IP'] = 'Για την επιλεγμένη περίοδο, αντιπροσωπεύει τον αριθμό των σελίδων και εικόνων που έχει δει μια διεύθυνση IP';
+$lang['AStat_Nfo_Category'] = 'Για την επιλεγμένη περίοδο, αντιπροσωπεύουν τοις εκατό του αριθμού των σελίδων και εικόνων σε επισκέψεις ανά κατηγορία, ως ο μέσος αριθμός εμφανίσεων ανά εικόνα';
+$lang['AStat_NbImgPerPages'] = 'Αριθμός εικόνων ανά σελίδα';
+$lang['AStat_NbIPPerPages'] = 'Αριθμός IP ανά σελίδα';
+$lang['AStat_NbCatPerPages'] = 'Αριθμός κατηγοριών ανά σελίδα';
+$lang['AStat_MouseOverColor'] = 'Σειρά Mouse over';
+$lang['AStat_MaxBarWidth'] = 'Μέγιστο πλάτος μπάρας (σε πίξελ)';
+$lang['AStat_IP_geolocalisation'] = 'Γεωεντοπισμός';
+$lang['AStat_IP_blacklist'] = 'Προσθήκη στη μαύρη λίστα';
+$lang['AStat_DelIP'] = 'Διαγραφή από τη μαύρη λίστα';
+$lang['AStat_DefaultSortImg'] = 'Προεπιλεγμένη ταξινόμηση';
+$lang['AStat_DefaultSortIP'] = 'Προεπιλεγμένη ταξινόμηση';
+$lang['AStat_DefaultSortCat'] = 'Προεπιλεγμένη ταξινόμηση';
+$lang['AStat_BlackListedIP'] = 'Μαύρη λίστα IP';
+$lang['AStat_BarColor_Pages'] = 'Αριθμός σελίδων';
+$lang['AStat_BarColor_Img'] = 'Αριθμός φωτογραφιών';
+$lang['AStat_BarColor_IP'] = 'Αριθμός των διευθύνσεων IP';
+$lang['AStat_BarColor_Cat'] = 'Αριθμός των κατηγοριών';
+$lang['AStat_AddIP'] = 'Προσθήκη στη μαύρη λίστα';
+?>
