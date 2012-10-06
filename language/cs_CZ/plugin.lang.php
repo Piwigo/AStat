b72c73919e920a6f1ff5420c154cddf7663e1747
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-06 06:24:52 +0000

    [astat] Update cs_CZ, thanks to : sichr
    git-svn-id: http://piwigo.org/svn/extensions/AStat@18495 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,131 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['AStat_tools_result_ko'] = 'Došlo k chybě. Akce nebyla korektně provedena.';
+$lang['AStat_tools_title'] = 'Nástroje údržby';
+$lang['AStat_tools_deleted_user_apply'] = 'Aktualizovat';
+$lang['AStat_tools_deleted_picture_apply'] = 'Aktualizovat';
+$lang['AStat_tools'] = 'Nástroje';
+$lang['AStat_tools_deleted_category'] = 'Aktualizovat kategorie v historii';
+$lang['AStat_tools_deleted_category_apply'] = 'Aktualizovat';
+$lang['AStat_tools_invalid_date2'] = 'Zadané datum je starší nebo rovno datu dne';
+$lang['AStat_tools_invalid_date'] = 'Zadané datum není platné!';
+$lang['AStat_tools_purge_history'] = 'Vyčištění historie';
+$lang['AStat_tools_purge_history_apply'] = 'Vyčistit';
+$lang['AStat_tools_result_ok'] = 'Akce správně provedena';
+$lang['pct_Pages_seen'] = '%Stran';
+$lang['pct_Pictures_seen'] = '%Obrázků';
+$lang['ratio_Pictures_seen'] = 'Zobrazených obrázků';
+$lang['AStat_version'] = 'verze';
+$lang['AStat_yesno_false'] = 'Ne';
+$lang['AStat_yesno_true'] = 'Ano';
+$lang['CATEGORY_LABEL'] = 'Kategorie';
+$lang['Categories_seen'] = 'Zobrazených kategorií';
+$lang['IP_label'] = 'IP adresa';
+$lang['IP_visit'] = 'IP adres';
+$lang['Pictures_seen'] = 'Zobrazených obrázků';
+$lang['AStat_sortcat_nbpicture'] = 'Průměrný počet zobrazení na obrázek';
+$lang['AStat_sortip_ip'] = 'Uživatel/IP adresy';
+$lang['AStat_sortip_page'] = 'Počet zobrazených stránek';
+$lang['AStat_sortip_picture'] = 'Počet zobrazených obrázků';
+$lang['AStat_title_page'] = 'Pokročilé statistiky';
+$lang['AStat_period_label_months'] = 'Měsíců';
+$lang['AStat_period_label_years'] = 'Roků';
+$lang['AStat_section_deleted_cat'] = 'Kategorie smazána';
+$lang['AStat_section_label'] = 'Sekce';
+$lang['AStat_nb_total_category'] = 'Celkový počet kategorií';
+$lang['AStat_nb_total_image'] = 'Celkový počet obrázků';
+$lang['AStat_nb_total_ip'] = 'Celkový počet IP adres';
+$lang['AStat_nofilter'] = '--- Žádný filtr ---';
+$lang['AStat_page_label'] = 'Stránka';
+$lang['AStat_pages_label'] = 'Stránky';
+$lang['AStat_period_label_all'] = 'Celý rok';
+$lang['AStat_period_label_days'] = 'Dnů';
+$lang['AStat_period_label_global'] = 'Globální';
+$lang['AStat_period_label_hours'] = 'Hodin';
+$lang['AStat_month_of_year_11'] = 'Listopad';
+$lang['AStat_month_of_year_12'] = 'Prosinec';
+$lang['AStat_month_of_year_2'] = 'Únor';
+$lang['AStat_month_of_year_3'] = 'Březen';
+$lang['AStat_month_of_year_4'] = 'Duben';
+$lang['AStat_month_of_year_5'] = 'Květen';
+$lang['AStat_month_of_year_6'] = 'Červen';
+$lang['AStat_month_of_year_7'] = 'Červenec';
+$lang['AStat_month_of_year_8'] = 'Srpen';
+$lang['AStat_month_of_year_9'] = 'Září';
+$lang['AStat_deleted_user'] = 'Smazaný uživatel';
+$lang['AStat_do_save'] = 'Uložit';
+$lang['AStat_enableddisabled_false'] = 'Filtr zrušen';
+$lang['AStat_enableddisabled_invert'] = 'Filtr invertován';
+$lang['AStat_enableddisabled_true'] = 'Filtr povolen';
+$lang['AStat_event'] = 'událost';
+$lang['AStat_events'] = 'události';
+$lang['AStat_general_config'] = 'Globální nastavení';
+$lang['AStat_month_of_year_1'] = 'Leden';
+$lang['AStat_month_of_year_10'] = 'Říjen';
+$lang['AStat_PeriodPerDefault'] = 'Standardní vybrané období';
+$lang['AStat_confignotsaved'] = 'Došlo k chybě, nastavení nebylo uloženo';
+$lang['AStat_date_time_format'] = 'R/d/m H:m:s';
+$lang['AStat_day_of_week_0'] = 'Neděle';
+$lang['AStat_day_of_week_1'] = 'Pondělí';
+$lang['AStat_day_of_week_2'] = 'Úterý';
+$lang['AStat_day_of_week_3'] = 'Středa';
+$lang['AStat_day_of_week_4'] = 'Čtvrtek';
+$lang['AStat_day_of_week_5'] = 'Pátek';
+$lang['AStat_day_of_week_6'] = 'Sobota';
+$lang['AStat_deleted_picture'] = 'Obrázek už neexistuje';
+$lang['AStat_SortImgLabel'] = 'Ukázat náhled obrázku';
+$lang['AStat_by_category'] = 'Dle kategorie';
+$lang['AStat_by_image'] = 'Dle obrázků';
+$lang['AStat_by_ip'] = 'Dle IP';
+$lang['AStat_by_period'] = 'Dle období';
+$lang['AStat_catfilter_list'] = 'Aplikovat na kategorie filtr';
+$lang['AStat_config'] = 'Nastavení';
+$lang['AStat_config_colors_and_graph'] = 'Nastavení barev a grafiky';
+$lang['AStat_config_saved'] = 'Nastavení uloženo!';
+$lang['AStat_config_title'] = 'Nastavení AStat';
+$lang['AStat_PeriodPerDefault_all'] = 'Všechny roky';
+$lang['AStat_PeriodPerDefault_day'] = 'Tento den';
+$lang['AStat_PeriodPerDefault_global'] = 'Globálně';
+$lang['AStat_PeriodPerDefault_month'] = 'Tento měsíc';
+$lang['AStat_PeriodPerDefault_year'] = 'Tento rok';
+$lang['AStat_RefIPLabel'] = 'Uživatel/IP adresa';
+$lang['AStat_RefImageLabel'] = 'Kategorie/Název obrázku';
+$lang['AStat_ShowThumbCat'] = 'Ukázat náhled zástupce kategorie';
+$lang['AStat_ShowThumbImg'] = 'Ukázat náhled obrázku';
+$lang['AStat_SortCatLabel'] = 'Řazeno dle';
+$lang['AStat_SortIPLabel'] = 'Řazeno dle';
+$lang['AStat_AddIP'] = 'Přidat na seznam blokovaných';
+$lang['AStat_BarColor_Cat'] = 'Počet kategorií';
+$lang['AStat_BarColor_IP'] = 'Počet IP adres';
+$lang['AStat_BarColor_Img'] = 'Počet obrázků';
+$lang['AStat_BarColor_Pages'] = 'Počet satránek';
+$lang['AStat_BlackListedIP'] = 'Blokovaná IP';
+$lang['AStat_DefaultSortCat'] = 'Standardní řazení';
+$lang['AStat_DefaultSortIP'] = 'Standardní řazení';
+$lang['AStat_DefaultSortImg'] = 'Standardní řazení';
+$lang['AStat_DelIP'] = 'Odstranit ze seznamu blokovaných';
+$lang['AStat_IP_blacklist'] = 'Přidat na seznam blokovaných';
+$lang['AStat_NbCatPerPages'] = 'Počet kategorií na stránku';
+$lang['AStat_NbIPPerPages'] = 'Počet IP na stránku';
+$lang['AStat_NbImgPerPages'] = 'Počet obrázků na stránku';
+?>
