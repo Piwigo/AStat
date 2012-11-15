d65238233667ee411140424e1839a2ff0b249b0f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-11-15 06:38:50 +0000

    [astat] Update ru_RU, thanks to : rocket
    git-svn-id: http://piwigo.org/svn/extensions/AStat@19016 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,116 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['AStat_section_additional_page'] = 'Плагин  <a href="http://phpwebgallery.net/ext/extension_view.php?eid=153">Additional pages</a>';
+$lang['AStat_section_deleted_cat'] = 'Категория удалена';
+$lang['AStat_section_label'] = 'Секция';
+$lang['AStat_section_most_commented'] = 'Плагин <a href="http://phpwebgallery.net/ext/extension_view.php?eid=145">Most Commented</a>';
+$lang['AStat_section_old_deleted_cat'] = 'Невозможно импортировать старую категорию';
+$lang['AStat_section_unknown'] = 'Неизвестный [%s]';
+$lang['AStat_section_web_services'] = 'Плагин <a href="http://phpwebgallery.net/ext/extension_view.php?eid=171">Web services statistics</a>';
+$lang['AStat_sortcat_nbpicture'] = 'Среднее число просмотров изображения';
+$lang['AStat_month_of_year_5'] = 'Май';
+$lang['AStat_month_of_year_6'] = 'Июнь';
+$lang['AStat_month_of_year_7'] = 'Июль';
+$lang['AStat_month_of_year_8'] = 'Август';
+$lang['AStat_month_of_year_9'] = 'Сентябрь';
+$lang['AStat_nb_total_category'] = 'Общее количество категорий';
+$lang['AStat_nb_total_image'] = 'Общее количество изображений';
+$lang['AStat_nb_total_ip'] = 'Общее количество IP адресов';
+$lang['AStat_nofilter'] = '---Без фильтра---';
+$lang['AStat_page_label'] = 'Страница';
+$lang['AStat_pages_label'] = 'Страницы';
+$lang['AStat_period_label_all'] = 'Весь год';
+$lang['AStat_period_label_days'] = 'Дни';
+$lang['AStat_period_label_global'] = 'Общие';
+$lang['AStat_period_label_hours'] = 'Часы';
+$lang['AStat_period_label_months'] = 'Месяцы';
+$lang['AStat_period_label_years'] = 'Года';
+$lang['AStat_deleted_picture'] = 'Картинка больше не существует';
+$lang['AStat_deleted_user'] = 'Удаленный пользователь';
+$lang['AStat_do_save'] = 'Сохранить';
+$lang['AStat_enableddisabled_false'] = 'Фильтр отключен';
+$lang['AStat_enableddisabled_invert'] = 'Инверсия фильтра';
+$lang['AStat_enableddisabled_true'] = 'Фильтр включен';
+$lang['AStat_event'] = 'событие';
+$lang['AStat_events'] = 'события';
+$lang['AStat_general_config'] = 'Общие настройки';
+$lang['AStat_gpc2_not_installed'] = 'Для установки AStat требуется установленный плагин\'Grum Plugins Classes 2\' (release >= %s) ';
+$lang['AStat_month_of_year_1'] = 'Январь';
+$lang['AStat_month_of_year_10'] = 'Октябрь';
+$lang['AStat_month_of_year_11'] = 'Ноябрь';
+$lang['AStat_month_of_year_12'] = 'Декабрь';
+$lang['AStat_month_of_year_2'] = 'Февраль';
+$lang['AStat_month_of_year_3'] = 'Март';
+$lang['AStat_month_of_year_4'] = 'Апрель';
+$lang['AStat_by_image'] = 'По изображению';
+$lang['AStat_by_ip'] = 'По IP';
+$lang['AStat_by_period'] = 'По периоду';
+$lang['AStat_catfilter_list'] = 'Применить фильтр на категории';
+$lang['AStat_config'] = 'Настройки';
+$lang['AStat_config_colors_and_graph'] = 'Настройки цвета и графики';
+$lang['AStat_config_saved'] = 'Настройки сохранены!';
+$lang['AStat_config_title'] = 'Настройки AStat';
+$lang['AStat_confignotsaved'] = 'Произошла ошибка: настройки не сохранены';
+$lang['AStat_date_time_format'] = 'Y/d/m H:i:s';
+$lang['AStat_day_of_week_0'] = 'Воскресенье';
+$lang['AStat_day_of_week_1'] = 'Понедельник';
+$lang['AStat_day_of_week_2'] = 'Вторник';
+$lang['AStat_day_of_week_3'] = 'Среда';
+$lang['AStat_day_of_week_4'] = 'Четверг';
+$lang['AStat_day_of_week_5'] = 'Пятница';
+$lang['AStat_day_of_week_6'] = 'Суббота';
+$lang['AStat_Nfo_Period'] = 'Показывает для выбранного периода число просмотров страниц и изображений от общего числа подключенных IP адресов';
+$lang['AStat_PeriodPerDefault'] = 'Пероид, выбранный по умолчанию';
+$lang['AStat_PeriodPerDefault_all'] = 'Все года';
+$lang['AStat_PeriodPerDefault_day'] = 'Текущий день';
+$lang['AStat_PeriodPerDefault_global'] = 'Глобальный';
+$lang['AStat_PeriodPerDefault_month'] = 'Текущий месяц';
+$lang['AStat_PeriodPerDefault_year'] = 'Текущий год';
+$lang['AStat_RefIPLabel'] = 'Пользователь\IP адрес';
+$lang['AStat_RefImageLabel'] = 'Категория\Название изображения';
+$lang['AStat_SeeTimeRequests'] = 'Показывать время выполнения запросов';
+$lang['AStat_ShowThumbCat'] = 'Показывать эскиз категории';
+$lang['AStat_ShowThumbImg'] = 'Показывать эскиз изображения';
+$lang['AStat_SortCatLabel'] = 'Отсортировано по';
+$lang['AStat_SortIPLabel'] = 'Отсортировано по';
+$lang['AStat_SortImgLabel'] = 'Отсортировано по';
+$lang['AStat_adviser_not_authorized'] = 'Действие отменено: это операция запрещена';
+$lang['AStat_by_category'] = 'По категории';
+$lang['AStat_AddIP'] = 'Добавить в черный список';
+$lang['AStat_BarColor_Cat'] = 'Количество категорий';
+$lang['AStat_BarColor_IP'] = 'Количество IP адресов';
+$lang['AStat_BarColor_Img'] = 'Количество картинок';
+$lang['AStat_BarColor_Pages'] = 'Количество страниц';
+$lang['AStat_BlackListedIP'] = 'IP из черного списка';
+$lang['AStat_DefaultSortCat'] = 'Сортировка по умолчанию';
+$lang['AStat_DefaultSortIP'] = 'Сортировка по умолчанию';
+$lang['AStat_DefaultSortImg'] = 'Сортировка по умолчанию';
+$lang['AStat_DelIP'] = 'Удалить из черного списка';
+$lang['AStat_IP_blacklist'] = 'Добавить в черный список';
+$lang['AStat_NbCatPerPages'] = 'Количество категорий на странице';
+$lang['AStat_NbIPPerPages'] = 'Количество IP адресов на странице';
+$lang['AStat_NbImgPerPages'] = 'Количество фотографий на странице';
+$lang['AStat_Nfo_Category'] = 'За выбранный период, представляет количество (в процентах) страниц и изображений просмотренных по категории, как среднее число просмотров на изображение';
+$lang['AStat_Nfo_IP'] = 'За выбранный период, представляет количество страниц и изображений просмотренных с IP адреса';
+$lang['AStat_Nfo_Image'] = 'За выбранный период, количество (в процентах) просмотров каждого изображения от общего количества просмотров';
+?>
