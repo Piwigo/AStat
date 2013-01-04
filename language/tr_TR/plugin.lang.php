40ce0cf5f82a951898b50d5d171f32d86c7753b3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-04 16:33:46 +0000

    [astat] Add tr_TR, thanks to : Nakre
    git-svn-id: http://piwigo.org/svn/extensions/AStat@19798 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,82 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['AStat_month_of_year_1'] = 'Ocak';
+$lang['AStat_month_of_year_10'] = 'Ekim';
+$lang['AStat_month_of_year_11'] = 'Kasım';
+$lang['AStat_month_of_year_12'] = 'Aralım';
+$lang['AStat_month_of_year_2'] = 'Şubat';
+$lang['AStat_month_of_year_3'] = 'Mart';
+$lang['AStat_month_of_year_4'] = 'Nisan';
+$lang['AStat_month_of_year_5'] = 'Mayıs';
+$lang['AStat_month_of_year_6'] = 'Haziran';
+$lang['AStat_month_of_year_7'] = 'Temmuz';
+$lang['AStat_month_of_year_8'] = 'Ağustos';
+$lang['AStat_month_of_year_9'] = 'Eylül';
+$lang['AStat_nb_total_category'] = 'Toplam kategori sayısı';
+$lang['AStat_nb_total_image'] = 'Toplam resim sayısı';
+$lang['AStat_nb_total_ip'] = 'Toplam ip adresi sayısı';
+$lang['AStat_page_label'] = 'Sayfa';
+$lang['AStat_pages_label'] = 'Sayfalar';
+$lang['AStat_section_deleted_cat'] = 'Kategori silindi';
+$lang['AStat_period_label_all'] = 'Tüm Yıl';
+$lang['AStat_period_label_days'] = 'Günler';
+$lang['AStat_period_label_hours'] = 'Saatler';
+$lang['AStat_period_label_months'] = 'Aylar';
+$lang['AStat_period_label_years'] = 'Yıllar';
+$lang['AStat_tools'] = 'Araçlar';
+$lang['AStat_tools_deleted_category_apply'] = 'Güncelle';
+$lang['AStat_sortip_page'] = 'Görüntülenen sayfa sayısı';
+$lang['AStat_sortip_picture'] = 'Görüntülenen resim sayısı';
+$lang['AStat_tools_deleted_picture_apply'] = 'Güncelle';
+$lang['AStat_tools_deleted_user_apply'] = 'Güncelle';
+$lang['AStat_tools_purge_history'] = 'Geçmiş siliniyor';
+$lang['AStat_tools_purge_history_apply'] = 'Sil';
+$lang['AStat_version'] = 'sürüm';
+$lang['AStat_yesno_false'] = 'Evet';
+$lang['AStat_yesno_true'] = 'Hayır';
+$lang['CATEGORY_LABEL'] = 'Kategori';
+$lang['IP_label'] = 'IP adresi';
+$lang['IP_visit'] = 'IP adresleri';
+$lang['AStat_do_save'] = 'Kaydet';
+$lang['AStat_day_of_week_1'] = 'Pazartesi';
+$lang['AStat_day_of_week_2'] = 'Salı';
+$lang['AStat_day_of_week_3'] = 'Çarşamba';
+$lang['AStat_day_of_week_4'] = 'Perşembe';
+$lang['AStat_day_of_week_5'] = 'Cuma';
+$lang['AStat_day_of_week_6'] = 'Cumartesi';
+$lang['AStat_SortCatLabel'] = 'tarafından düzenlendi';
+$lang['AStat_SortIPLabel'] = 'tarafından düzenlendi';
+$lang['AStat_SortImgLabel'] = 'tarafından düzenlendi';
+$lang['AStat_config'] = 'Ayarlar';
+$lang['AStat_config_saved'] = 'Ayarlar kaydedildi';
+$lang['AStat_config_title'] = 'AStat Ayarları';
+$lang['AStat_day_of_week_0'] = 'Pazar';
+$lang['AStat_PeriodPerDefault_year'] = 'Bu yıl';
+$lang['AStat_PeriodPerDefault_month'] = 'Bu ay';
+$lang['AStat_PeriodPerDefault_all'] = 'Tüm yıllar';
+$lang['AStat_PeriodPerDefault_day'] = 'Bugün';
+$lang['AStat_BarColor_Cat'] = 'Kategori sayısı';
+$lang['AStat_BarColor_IP'] = 'Ip adresi sayısı';
+$lang['AStat_BarColor_Img'] = 'Resim sayısı';
+$lang['AStat_BarColor_Pages'] = 'Sayfa sayısı';
+?>
