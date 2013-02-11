fe707cad80416d51c1fb8167f88932d7166609c9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-11 20:01:57 +0000

    [astat] Add pl_PL, thanks to : K.S.
    git-svn-id: http://piwigo.org/svn/extensions/AStat@20673 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,111 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['AStat_tools_deleted_category'] = 'Zaktualizuj kategorie w historii';
+$lang['AStat_gpc2_not_installed'] = 'Wtyczka \'Grum Plugins Classes 2\' (release >= %s) jest wymagana do instalacji AStat';
+$lang['AStat_month_of_year_1'] = 'Styczeń';
+$lang['AStat_month_of_year_10'] = 'Październik';
+$lang['AStat_month_of_year_11'] = 'Listopad';
+$lang['AStat_month_of_year_12'] = 'Grudzień';
+$lang['AStat_month_of_year_2'] = 'Luty';
+$lang['AStat_month_of_year_3'] = 'Marzec';
+$lang['AStat_month_of_year_4'] = 'Kwiecień';
+$lang['AStat_month_of_year_5'] = 'Maj';
+$lang['AStat_month_of_year_6'] = 'Czerwiec';
+$lang['AStat_month_of_year_7'] = 'Lipiec';
+$lang['AStat_month_of_year_8'] = 'Sierpień';
+$lang['AStat_month_of_year_9'] = 'Wrzesień';
+$lang['AStat_nb_total_category'] = 'Całkowita liczba kategorii';
+$lang['AStat_nb_total_image'] = 'Całkowita liczba obrazów';
+$lang['AStat_nb_total_ip'] = 'Całkowita liczba adresów IP';
+$lang['AStat_nofilter'] = '--- brak filtra ---';
+$lang['AStat_page_label'] = 'Strona';
+$lang['AStat_pages_label'] = 'Strony';
+$lang['AStat_period_label_all'] = 'Wszystkie lata';
+$lang['AStat_period_label_days'] = 'Dni';
+$lang['AStat_period_label_hours'] = 'Godzin';
+$lang['AStat_period_label_months'] = 'Miesięcy';
+$lang['AStat_period_label_years'] = 'Lat';
+$lang['AStat_section_additional_page'] = 'Wtyczka <a href="http://phpwebgallery.net/ext/extension_view.php?eid=153">Additional pages</a>';
+$lang['AStat_section_most_commented'] = 'Wtyczka <a href="http://phpwebgallery.net/ext/extension_view.php?eid=145">Most Commented</a>';
+$lang['AStat_section_deleted_cat'] = 'Kategoria usunięta';
+$lang['AStat_section_label'] = 'Sekcja';
+$lang['AStat_section_web_services'] = 'Wtyczka <a href="http://phpwebgallery.net/ext/extension_view.php?eid=171">Web services statistics</a>';
+$lang['AStat_sortip_ip'] = 'Użytkownik / Adres IP';
+$lang['AStat_time_request_label'] = 'Żądanie wykonane w';
+$lang['AStat_title_page'] = 'Statystyki zaawansowane';
+$lang['AStat_tools'] = 'Narzędzia';
+$lang['AStat_tools_invalid_date'] = 'Podana data nie jest prawidłowa!';
+$lang['AStat_tools_purge_history'] = 'Czyszczenie historii';
+$lang['AStat_version'] = 'wersja';
+$lang['AStat_yesno_false'] = 'Nie';
+$lang['AStat_yesno_true'] = 'Tak';
+$lang['CATEGORY_LABEL'] = 'Kategoria';
+$lang['IP_label'] = 'Adres IP';
+$lang['IP_visit'] = 'Adresy IP';
+$lang['AStat_event'] = 'wydarzenie';
+$lang['AStat_events'] = 'wydarzenia';
+$lang['AStat_general_config'] = 'Ustawienia globalne';
+$lang['AStat_DefaultSortCat'] = 'Sortowanie domyślne';
+$lang['AStat_DefaultSortIP'] = 'Sortowanie domyślne';
+$lang['AStat_DefaultSortImg'] = 'Sortowanie domyślne';
+$lang['AStat_NbCatPerPages'] = 'Liczba kategorii na stronę';
+$lang['AStat_SortCatLabel'] = 'Posortowane wg';
+$lang['AStat_SortIPLabel'] = 'Posortowane wg';
+$lang['AStat_SortImgLabel'] = 'Posortowane wg';
+$lang['AStat_by_category'] = 'Wg kategorii';
+$lang['AStat_by_ip'] = 'Wg IP';
+$lang['AStat_catfilter_list'] = 'Zastosuj filtr na kategoriach';
+$lang['AStat_config'] = 'Ustawienia';
+$lang['AStat_config_colors_and_graph'] = 'Kolor & ustawienia graficzne';
+$lang['AStat_config_saved'] = 'Ustawienia zapisane!';
+$lang['AStat_config_title'] = 'Ustawienia AStat';
+$lang['AStat_confignotsaved'] = 'Wystąpił błąd, ustawienia nie zostały zapisane';
+$lang['AStat_date_time_format'] = 'Y-m-d H:i:s';
+$lang['AStat_day_of_week_0'] = 'Niedziela';
+$lang['AStat_day_of_week_1'] = 'Poniedziałek';
+$lang['AStat_day_of_week_2'] = 'Wtorek';
+$lang['AStat_day_of_week_3'] = 'Środa';
+$lang['AStat_day_of_week_4'] = 'Czwartek';
+$lang['AStat_day_of_week_5'] = 'Piątek';
+$lang['AStat_day_of_week_6'] = 'Sobota';
+$lang['AStat_deleted_picture'] = 'Obraz już nie istnieje';
+$lang['AStat_deleted_user'] = 'Usunięty użytkownik';
+$lang['AStat_do_save'] = 'Zapisz';
+$lang['AStat_PeriodPerDefault_all'] = 'Wszystkie lata';
+$lang['AStat_PeriodPerDefault_day'] = 'Dzień bieżący';
+$lang['AStat_PeriodPerDefault_month'] = 'Miesiąc bieżący';
+$lang['AStat_PeriodPerDefault_year'] = 'Rok bieżący';
+$lang['AStat_RefIPLabel'] = 'Użytkownik / Adres IP';
+$lang['AStat_RefImageLabel'] = 'Kategoria / Nazwa obrazu';
+$lang['AStat_SeeTimeRequests'] = 'Pokaż czas wykonania żądań';
+$lang['AStat_AddIP'] = 'Dodaj do czarnej listy';
+$lang['AStat_BarColor_Cat'] = 'Liczba kategorii';
+$lang['AStat_BarColor_IP'] = 'Liczba adresów IP';
+$lang['AStat_BarColor_Img'] = 'Liczba obrazów';
+$lang['AStat_BarColor_Pages'] = 'Liczba stron';
+$lang['AStat_DelIP'] = 'Usuń z czarnej listy';
+$lang['AStat_IP_blacklist'] = 'Dodaj do czarnej listy';
+$lang['AStat_IP_geolocalisation'] = 'Geolokalizacja';
+$lang['AStat_NbIPPerPages'] = 'Liczba adresów IP na stronę';
+$lang['AStat_NbImgPerPages'] = 'Liczba obrazów na stronę';
+?>
