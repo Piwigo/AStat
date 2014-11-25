22f67d694e9126b430745554ed197739ca83d1ef
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-25 06:03:11 +0000

    [astats] Update zh_CN, thanks to vicmilan
    git-svn-id: http://piwigo.org/svn/extensions/AStat@30590 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,110 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['AStat_sortip_page'] = '页面被阅览数';
+$lang['AStat_sortip_picture'] = '图片被阅览数';
+$lang['AStat_sortcat_nbpicture'] = '每张图片平均阅览数';
+$lang['AStat_sortcat_page'] = '按页面阅览次数百分比展示';
+$lang['AStat_sortcat_picture'] = '按图片阅览次数百分比展示';
+$lang['AStat_sortimg_picture'] = '图片被阅览数';
+$lang['AStat_sortip_ip'] = '用户/IP地址';
+$lang['AStat_period_label_all'] = '所有年份';
+$lang['AStat_period_label_days'] = '天';
+$lang['AStat_period_label_global'] = '全局';
+$lang['AStat_period_label_hours'] = '小时';
+$lang['AStat_period_label_months'] = '月';
+$lang['AStat_period_label_years'] = '年';
+$lang['AStat_section_deleted_cat'] = '分类已删除';
+$lang['AStat_section_label'] = '区域';
+$lang['AStat_month_of_year_5'] = '五月';
+$lang['AStat_month_of_year_6'] = '六月';
+$lang['AStat_month_of_year_7'] = '七月';
+$lang['AStat_month_of_year_8'] = '八月';
+$lang['AStat_month_of_year_9'] = '九月';
+$lang['AStat_nb_total_category'] = '分类总数';
+$lang['AStat_nb_total_image'] = '图片总数';
+$lang['AStat_nb_total_ip'] = 'IP地址总数';
+$lang['AStat_nofilter'] = '无过滤器';
+$lang['AStat_page_label'] = '页';
+$lang['AStat_pages_label'] = '页';
+$lang['AStat_enableddisabled_true'] = '过滤器启用';
+$lang['AStat_event'] = '事件';
+$lang['AStat_events'] = '事件';
+$lang['AStat_general_config'] = '全局设置';
+$lang['AStat_month_of_year_1'] = '一月';
+$lang['AStat_month_of_year_10'] = '十月';
+$lang['AStat_month_of_year_11'] = '十一月';
+$lang['AStat_month_of_year_12'] = '十二月';
+$lang['AStat_month_of_year_2'] = '二月';
+$lang['AStat_month_of_year_3'] = '三月';
+$lang['AStat_month_of_year_4'] = '四月';
+$lang['AStat_day_of_week_0'] = '星期天';
+$lang['AStat_day_of_week_1'] = '星期一';
+$lang['AStat_day_of_week_2'] = '星期二';
+$lang['AStat_day_of_week_3'] = '星期三';
+$lang['AStat_day_of_week_4'] = '星期四';
+$lang['AStat_day_of_week_5'] = '星期五';
+$lang['AStat_day_of_week_6'] = '星期六';
+$lang['AStat_deleted_picture'] = '图片已不存在';
+$lang['AStat_deleted_user'] = '删除用户';
+$lang['AStat_do_save'] = '保存';
+$lang['AStat_enableddisabled_false'] = '过滤器禁用';
+$lang['AStat_enableddisabled_invert'] = '过滤器反转';
+$lang['AStat_BarColor_Cat'] = '分类数量';
+$lang['AStat_SortImgLabel'] = '根据...分类';
+$lang['AStat_adviser_not_authorized'] = '操作中止:顾问档案不允许此类操作';
+$lang['AStat_by_category'] = '根据分类';
+$lang['AStat_by_image'] = '每张图片';
+$lang['AStat_by_ip'] = '根据IP';
+$lang['AStat_by_period'] = '根据时期';
+$lang['AStat_catfilter_list'] = '在类别上应用过滤器';
+$lang['AStat_config'] = '设置';
+$lang['AStat_config_colors_and_graph'] = '颜色&图像设置';
+$lang['AStat_config_saved'] = '设置已保存!';
+$lang['AStat_confignotsaved'] = '出现一个错误, 设置未保存';
+$lang['AStat_PeriodPerDefault'] = '默认选择时期';
+$lang['AStat_PeriodPerDefault_all'] = '所有年份';
+$lang['AStat_PeriodPerDefault_day'] = '当天';
+$lang['AStat_PeriodPerDefault_global'] = '全局';
+$lang['AStat_PeriodPerDefault_month'] = '当月';
+$lang['AStat_PeriodPerDefault_year'] = '当年';
+$lang['AStat_RefIPLabel'] = '用户/IP地址';
+$lang['AStat_RefImageLabel'] = '分类/图片名';
+$lang['AStat_SeeTimeRequests'] = '显示要求的执行时间';
+$lang['AStat_ShowThumbCat'] = '显示代表分类的缩略图';
+$lang['AStat_ShowThumbImg'] = '显示图片的缩略图';
+$lang['AStat_SortCatLabel'] = '根据...分类';
+$lang['AStat_SortIPLabel'] = '根据...分类';
+$lang['AStat_AddIP'] = '添加到黑名单';
+$lang['AStat_BarColor_IP'] = 'IP地址数量';
+$lang['AStat_BarColor_Img'] = '图片数量';
+$lang['AStat_BarColor_Pages'] = '页面数量';
+$lang['AStat_BlackListedIP'] = 'IP黑名单';
+$lang['AStat_DefaultSortCat'] = '默认分类';
+$lang['AStat_DefaultSortIP'] = '默认分类';
+$lang['AStat_DefaultSortImg'] = '默认分类';
+$lang['AStat_DelIP'] = '从黑名单中删除';
+$lang['AStat_IP_blacklist'] = '添加到黑名单';
+$lang['AStat_MaxBarWidth'] = '最大宽度(像素)';
+$lang['AStat_NbCatPerPages'] = '每页类别数量';
+$lang['AStat_NbIPPerPages'] = '每页IP数量';
+$lang['AStat_NbImgPerPages'] = '每页图片数量';
