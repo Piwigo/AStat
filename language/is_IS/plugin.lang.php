f4574fcca249bc213bba12a3fe920db3c645402a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-16 20:51:32 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['AStat_AddIP'] = 'Bæta við svartan lista';
+$lang['AStat_BarColor_Cat'] = 'Fjöldi flokka';
+$lang['AStat_BarColor_IP'] = 'Fjöldi IP talna';
+$lang['AStat_BarColor_Img'] = 'Fjöldi mynda';
+$lang['AStat_BarColor_Pages'] = 'Fjöldi síðna';
+$lang['AStat_BlackListedIP'] = 'IP á svörtum lista';
+$lang['AStat_DefaultSortCat'] = 'Sjálfgefin röðun';
+$lang['AStat_DefaultSortIP'] = 'Sjálfgefin röðun';
+$lang['AStat_DefaultSortImg'] = 'Sjálfgefin röðun';
+$lang['AStat_DelIP'] = 'Eyða af svörtum lista';
+$lang['AStat_IP_blacklist'] = 'Bæta við svartan lista';
